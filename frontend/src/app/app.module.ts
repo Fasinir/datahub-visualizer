025b1a9a09b2efb7194c3641f1a00ac3cd471cdb
Daniel Rosa <daniel_rosa@epam.com>
2022-04-06 10:43:47 +0200

    Add main view draft

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { ContentComponent } from './content/content.component';
+import { TileComponent } from './content/tile/tile.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './content/menu/menu.component';
+import { MenuChartChoiceComponent } from './content/menu/menu-chart-choice/menu-chart-choice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    ContentComponent,
+    TileComponent,
+    FooterComponent,
+    MenuComponent,
+    MenuChartChoiceComponent
   ],
   imports: [
     BrowserModule,
