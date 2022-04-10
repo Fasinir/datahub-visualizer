d90ffcf4b057180fc009f81ee9a77c026151ebf3
Fasinir <filipson17@gmail.com>
2022-04-10 17:53:56 +0200

    upload button added to banner

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoadFileButtonComponent } from './components/load-file-button/load-file-button.component';
 
 
-const routes: Routes = [
-  { path: 'loadConfig', component: LoadFileButtonComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
