9e07371ed6a07d7fc39926bf5631c9a3027df72f
Daniel Rosa <danielrosadev@gmail.com>
2022-05-18 11:55:15 +0200

    Small cleanup

--- frontend/src/app/content/content.component.ts
+++ frontend/src/app/content/content.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostListener} from '@angular/core';
+import {Component} from '@angular/core';
 import {Subscription} from 'rxjs';
 import {ChartService} from '../services/chart.service';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
@@ -42,18 +42,7 @@ export class ContentComponent {
     modalRef.componentInstance.outliers = outliers;
   }
 
-  @HostListener('drop', ['$event']) 
-  onDrop(event: DragEvent) {
-    event.preventDefault();
-    event.stopPropagation();
-    console.log(event.dataTransfer);
-  }
-  
-
-  chartType(type
-              :
-              string
-  ) {
+  chartType(type: string) {
     if (type === "LINE_BAR_CHART") {
       return "arrow";
     }
--- frontend/src/app/content/tile/tile.component.ts
+++ frontend/src/app/content/tile/tile.component.ts
@@ -1,16 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-tile',
   templateUrl: './tile.component.html',
   styleUrls: ['./tile.component.css']
 })
-export class TileComponent implements OnInit {
-
+export class TileComponent {
   @Input() chartType: string = "placeholder";
   @Input() chartTitle: string = "Wykres";
-
-  ngOnInit(): void {
-  }
-
 }
