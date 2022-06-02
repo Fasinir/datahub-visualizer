f0b5807f95acd62a655f300a05c225fba7c3564d
Fasinir <filipson17@gmail.com>
2022-06-02 15:18:00 +0200

    removed if

--- frontend/src/app/content/content.component.ts
+++ frontend/src/app/content/content.component.ts
@@ -1,12 +1,12 @@
-import {Component} from '@angular/core';
-import {Subscription} from 'rxjs';
-import {ChartService} from '../services/chart.service';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {ModalComponent} from "./modal/modal.component";
-import {ChartDataset} from "chart.js";
-import {Outlier} from "./chart/outliers.model";
-import {MenuService} from "../services/menu.service";
-import {MobileData} from "./mobile-data";
+import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ChartService } from '../services/chart.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from "./modal/modal.component";
+import { ChartDataset } from "chart.js";
+import { Outlier } from "./chart/outliers.model";
+import { MenuService } from "../services/menu.service";
+import { MobileData } from "./mobile-data";
 
 @Component({
   selector: 'app-content',
@@ -28,7 +28,7 @@ export class ContentComponent {
   menuSubscription: Subscription = this.menuService.clickedChartData
     .subscribe(data => {
       this.mobileData.isSingleValue = data.type === 'SINGLE_VALUE_CHART';
-      if (this.mobileData.isSingleValue){
+      if (this.mobileData.isSingleValue) {
         this.mobileData.singleValue = data.value
         this.mobileData.name = data.label;
       }
@@ -45,14 +45,9 @@ export class ContentComponent {
     let colors: string[] = [];
 
     for (let set of data.chartData) {
-      chartDatasets.push({label: set.label, data: set.yVals, type: set.type.toLowerCase(), spanGaps: true})
-      outliers.push({outlierLow: set.outlierLow, outlierHigh: set.outlierHigh})
-
-      if(set.hasOwnProperty('color')){
+      chartDatasets.push({ label: set.label, data: set.yVals, type: set.type.toLowerCase(), spanGaps: true })
+      outliers.push({ outlierLow: set.outlierLow, outlierHigh: set.outlierHigh })
       colors.push(set.color)
-      } else {
-        colors.push("lightblue")
-      }
     }
     this.mobileData.name = data.label;
     this.mobileData.labels = data.xVals;
